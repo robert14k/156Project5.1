69273db0f7cd97097bd8ed73c22a84e93484cffd
robert14k <35847872+robert14k@users.noreply.github.com>
2018-11-17 16:35:00 -0600

    added list files
    added the list node files and put them in the datacontiners and fix the brakets being in the wrong spot and tabbed over them thods so they are now easier to read and cleaner

---
+++ src/datacontainers/InvoiceList.java
@@ -0,0 +1,34 @@
+package datacontainers;
+
+public class InvoiceList {
+
+
+	public void clear() {
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+	
+	public void addToStart(Invoice t) {
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+	
+	public void addToEnd(Invoice t) {
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+	
+	public void remove(int position) {
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+	
+	private InvoiceListNode getInvoiceListNode(int position) {
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+	
+	public Invoice getInvoice(int position) {
+		throw new UnsupportedOperationException("Not yet implemented.");    	
+	}
+	
+	public void print() {
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+}
